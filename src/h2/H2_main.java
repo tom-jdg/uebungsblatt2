24bca84b68a0a79fd7c732e40c9a7ddf27f596de
tom-jdg <tom.jedig@gmail.com>
2025-10-26 15:55:07 +0100

    Hausaufgabe 2 bearbeitet

---
+++ src/h2/H2_main.java
@@ -0,0 +1,9 @@
+package h2;
+
+public class H2_main {
+	public static void main(String[] args) {
+		int i=1, j=2, k=3, min=4, max=5;
+		min = Math.min(i, Math.min(j, k));
+		max = Math.max(i, Math.max(j, k));
+	}
+}
