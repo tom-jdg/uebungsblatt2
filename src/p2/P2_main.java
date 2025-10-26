71f326df942dbad8d2ac8864457ac2675aa932fd
tom-jdg <tom.jedig@gmail.com>
2025-10-26 15:48:15 +0100

    Präsenzaufgaben 2 und 3 bearbeitet

---
+++ src/p2/P2_main.java
@@ -0,0 +1,9 @@
+package p2;
+
+public class P2_main {
+	public static void main(String[] args) {
+		int i = 2;
+		boolean b = false;
+		b = i > 10;
+	}
+}
---
+++ src/p3/P3_main.java
@@ -0,0 +1,16 @@
+package p3;
+
+public class P3_main {
+	public static void main(String[] args) {
+		int i=1, j=2, k=3;
+		if (i<j) {
+			System.out.println("A");
+		};
+		if (i<k) {
+			System.out.println("B");
+		};
+		if (j<k) {
+			System.out.println("C");
+		};
+	}
+}
